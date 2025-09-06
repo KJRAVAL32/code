5b768888be942c36d8d4a82cd2d6191986d27023
KJRAVAL32 <kuldeepr.reliution@gmail.com>
2025-09-06 13:17:16 +0530

    Update programs2.py

--- programs2.py
+++ programs2.py
@@ -5,22 +5,25 @@ class product:
         self.categeoy = categeoy
         self.price = int(price)
 class Category:
-    def __init__(self,name,code,parent=None):
+    def __init__(self,name,code,parent=''):
         self.name=name
         self.code = code
         self.parent=parent
         self.display_name=self._get_display_name()
 
-    def _get_display_name(self):
-        if self.parent:
-            return self.parent,">",self.name
-        else:
-            return self.name
+    def _get_display_name(self, child_name=None):
+        # if self.parent and child_name != '':
+            if self.parent:
+                # child_name = self.parent.name
+                return f"{self.parent.name}>{self.name}"
+            else:
+                return self.name
+        # elif self.parent and child_name == '':
+        #     if self.parent.parent:
+        #         child_name = self.parent.name
+        #         return f"{self.parent._get_display_name(child_name)} > {self.parent.name} > {self.name}"
 c1=Category("vehicle",1)
 c2=Category("car",2,c1)
-c3=Category("disel",3,c2)
-c4=Category("power",4,c3)
-c1._get_display_name()
-c2._get_display_name()
-c3._get_display_name()
-c4._get_display_name()
+c3=Category("disel",2,c2)
+# print(c1.display_name)
+print(c3.display_name)
